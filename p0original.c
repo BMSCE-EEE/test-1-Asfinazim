fba15f44c9c6ceeb6bed47199758a35f758dd6e8
asfi nazim <>
2021-11-26 03:20:11 +0000

    removed two scanf statements and made it one statement

--- p0original.c
+++ p0original.c
@@ -1,1 +1,13 @@
- 
+ #include<stdio.h>
+int main(void)
+{
+  int x,y,sum;
+  printf("enter the 2 numbers to be added: \n");
+  scanf("%d %d",&x,&y);
+  sum=x+y;
+  printf("sum of 2 numbers are= %d\n",sum);
+  return (0);
+}
+
+
+  
